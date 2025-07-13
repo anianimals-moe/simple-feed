1684d99b87b42f83eddce21a81af49c72d11f70a
neko-shinshi <admin@anianimals.moe>
2025-07-13 10:10:30 +0800

    Update prunePosts.ts

--- src/subroutines/prunePosts.ts
+++ src/subroutines/prunePosts.ts
@@ -6,4 +6,7 @@ export function prunePosts(db) {
     const old = db.prepare('SELECT COUNT(1) AS count FROM posts').get().count;
     const info = db.prepare('DELETE FROM posts WHERE expires = 1 AND indexed_at < ?').run(earlier);
     console.log(`Posts has ${old} -> ${old-info.changes}(-${info.changes}) entries`);
+    
+    const feedCount = db.prepare('SELECT rkey, COUNT(_id) AS count FROM posts GROUP BY rkey').all();
+    console.log(feedCount);
 }
